72257727f1664732c9d4783627b9ec18d6c3c21b
Anozie <anozie804@gmail.com>
2025-05-06 19:44:22 +0100

    Update cylo.py

--- cylo.py
+++ cylo.py
@@ -1,7 +1,7 @@
 import requests
 import urllib.parse
 
-__ENDPOINT_URL__: str = "https://cpmcheats.abccloud.com.br/api2"
+__ENDPOINT_URL__: str = "https://cpmazraeldevs.squareweb.app/api/"
 
 class Bubcyz:
     def __init__(self, access_key) -> None:
